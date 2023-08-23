9452fb13edb3e5833c095a4ad25fff8227242170
Aadit Noronha <aadit.noronha@gmail.com>
2023-08-22 21:24:35 -0700

    Code that Renisha made

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeTele.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class arcadeTele extends LinearOpMode {
+    DcMotorEx leftFront;
+    DcMotorEx rightFront;
+    DcMotorEx leftBack;
+    DcMotorEx rightBack;
+
+    public void runOpMode() {
+
+        // initialize and configure here
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        // reverse any motors that should be reversed
+
+        waitForStart(); // so that the code doesn't end before the start button is clicked
+
+        while (opModeIsActive()) {
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.right_stick_x;
+            leftFront.setPower(y + x);
+            leftBack.setPower(y + x);
+            rightFront.setPower(y - x);
+            rightBack.setPower(y - x);
+        }
+    }
+}
